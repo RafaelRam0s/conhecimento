7e73005c8c4edeaf0b38a35b4ebbd0bf83086b39
Rafael <rafae@LAPTOP-1305CN88>
2020-10-30 16:31:08 -0300

    javascript tipos primitivos

--- htmlcssjavascript/_logicas/sorteio_de_numero/sorteio_de_um_numero_ate_outro_escolhido.js
+++ htmlcssjavascript/_logicas/sorteio_de_numero/sorteio_de_um_numero_ate_outro_escolhido.js
@@ -30,5 +30,52 @@ function sorteioAlcance(min, max) {
 }
 
 
+function sortearNumerosEntre(min, max) {
+	let numMin = 0, numMax = 0;
+    if (min < max) {
+    	numMin = min;
+        numMax = max;
+    } else {
+    	numMin = max;
+        numMax = min;
+	}
+    
+	let raio = Math.abs(numMax - numMin + 1);
+    let numSorteado = numMin + Math.floor(Math.random() * raio);
+    
+    return numSorteado;
+}
+
+
+
+
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+
+// <button onclick="sortearNumerosEntre(1, 100)">Tente</button>
+
+// <script>
+// function sortearNumerosEntre(min, max) {
+// 	let numMin = 0, numMax = 0;
+//     if (min < max) {
+//     	numMin = min;
+//         numMax = max;
+//     } else {
+//     	numMin = max;
+//         numMax = min;
+// 	}
+    
+// 	let raio = Math.abs(numMax - numMin + 1);
+//     let sorteado = numMin + Math.floor(Math.random() * raio);
+    
+//     window.alert(sorteado);
+// }
+// </script>
+
+// </body>
+// </html>
+
 
 
